b45687745e55e6c5e5a39283e768727e431551a7
Noah Crowley <noahwillcrow@gmail.com>
2022-03-23 11:52:57 -0400

    Update with base class and interface changes

--- src/classes/lists/CircularSinglyLinkedList.ts
+++ src/classes/lists/CircularSinglyLinkedList.ts
@@ -1,8 +1,6 @@
-import { SinglyLinkedListNode } from "classes/nodes/SinglyLinkedListNode";
 import { IReadonlyLinkedList } from "interfaces/IReadonlyLinkedList";
 import { NodeValue } from "types/NodeValue";
 import { SinglyLinkedList } from "classes/lists/SinglyLinkedList";
-import { ISinglyLinkedListNode } from "interfaces/ISinglyLinkedListNode";
 
 export class CircularSinglyLinkedList<T extends NodeValue> extends SinglyLinkedList<T> {
 	public copyLinkedListValuesToHead(valuesList: IReadonlyLinkedList<T>) {
@@ -21,25 +19,24 @@ export class CircularSinglyLinkedList<T extends NodeValue> extends SinglyLinkedL
 		}
 	}
 
-	public isLengthGreaterThanOrEqualTo(minLength: number) {
-		if (minLength <= 0) {
-			throw `Invalid input to isLengthGreaterThanOrEqualTo: ${minLength}`;
+	public moveNodesFromSinglyLinkedListToHead(otherSinglyLinkedList: SinglyLinkedList<T>) {
+		if (otherSinglyLinkedList.isEmpty()) {
+			return;
 		}
 
-		let numberOfNodesSeen = 0;
+		super.moveNodesFromSinglyLinkedListToHead(otherSinglyLinkedList);
 
-		for (const [_, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			numberOfNodesSeen++;
-			if (numberOfNodesSeen === minLength) {
-				return true;
-			}
+		this.tailNode!.nextNode = this.headNode;
+	}
 
-			if (currentNode === this.tailNode) {
-				break;
-			}
+	public moveNodesFromSinglyLinkedListToTail(otherSinglyLinkedList: SinglyLinkedList<T>) {
+		if (otherSinglyLinkedList.isEmpty()) {
+			return;
 		}
 
-		return false;
+		super.moveNodesFromSinglyLinkedListToTail(otherSinglyLinkedList);
+
+		this.tailNode!.nextNode = this.headNode;
 	}
 
 	public peekValueAtIndex(index: number) {
@@ -47,10 +44,11 @@ export class CircularSinglyLinkedList<T extends NodeValue> extends SinglyLinkedL
 			throw `Index was less than 0`;
 		}
 
-		let numberOfNodesSeen = 0;
-		for (const [currentIndex, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			numberOfNodesSeen++;
+		if (index > this.numberOfNodes) {
+			throw `Provided index, ${index}, is out of range of list with ${this.numberOfNodes} elements`;
+		}
 
+		for (const [currentIndex, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
 			if (currentIndex === index) {
 				return currentNode.value;
 			}
@@ -60,7 +58,8 @@ export class CircularSinglyLinkedList<T extends NodeValue> extends SinglyLinkedL
 			}
 		}
 
-		throw `Index ${index} was out of range of list with ${numberOfNodesSeen} elements`;
+		// should never get here
+		throw `Somehow failed to find index, ${index}, even though it is in bounds`;
 	}
 
 	public popHeadValue() {
@@ -79,6 +78,8 @@ export class CircularSinglyLinkedList<T extends NodeValue> extends SinglyLinkedL
 			this.headNode = oldHeadNode.nextNode;
 			this.tailNode!.nextNode = this.headNode;
 		}
+
+		this.numberOfNodes--;
 
 		return headValue;
 	}
@@ -108,49 +109,9 @@ export class CircularSinglyLinkedList<T extends NodeValue> extends SinglyLinkedL
 			this.tailNode!.nextNode = this.headNode;
 		}
 
+		this.numberOfNodes--;
+
 		return tailValue;
-	}
-
-	public popValueAtIndex(index: number) {
-		if (index <= 1) {
-			throw `Provided index, ${index}, is less than 0`;
-		}
-
-		let numberOfNodesSeen = 0;
-		let previousNode: ISinglyLinkedListNode<T> | undefined;
-		for (const [currentIndex, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			numberOfNodesSeen++;
-
-			if (currentIndex === index) {
-				const value = currentNode.value;
-
-				if (previousNode === undefined) {
-					// index must have been 1, which is the same as popping the head node
-					this.headNode = currentNode.nextNode;
-				} else {
-					previousNode.nextNode = currentNode.nextNode;
-				}
-
-				if (currentNode.nextNode === undefined) {
-					// currentNode was the tail node
-					this.tailNode = previousNode;
-				}
-
-				if (this.headNode !== undefined && this.tailNode !== undefined) {
-					this.tailNode.nextNode = this.headNode;
-				}
-
-				return value;
-			} else {
-				previousNode = currentNode;
-			}
-
-			if (currentNode === this.tailNode) {
-				break;
-			}
-		}
-
-		throw `Provided index, ${index}, is out of range of list with ${numberOfNodesSeen} elements`;
 	}
 
 	public pushArrayToHead(valuesArray: readonly T[]) {
@@ -158,25 +119,7 @@ export class CircularSinglyLinkedList<T extends NodeValue> extends SinglyLinkedL
 			return;
 		}
 
-		let mostRecentNode: SinglyLinkedListNode<T> | undefined;
-
-		for (const value of valuesArray) {
-			const newestNode = new SinglyLinkedListNode(value);
-
-			if (mostRecentNode === undefined) {
-				newestNode.nextNode = this.headNode;
-				this.headNode = newestNode;
-			} else {
-				newestNode.nextNode = mostRecentNode.nextNode;
-				mostRecentNode.nextNode = newestNode;
-			}
-
-			mostRecentNode = newestNode;
-		}
-
-		if (this.tailNode === undefined) {
-			this.tailNode = mostRecentNode;
-		}
+		super.pushArrayToHead(valuesArray);
 
 		this.tailNode!.nextNode = this.headNode;
 	}
@@ -186,93 +129,20 @@ export class CircularSinglyLinkedList<T extends NodeValue> extends SinglyLinkedL
 			return;
 		}
 
-		for (const value of valuesArray) {
-			const newNode = new SinglyLinkedListNode(value);
-
-			if (this.tailNode === undefined) {
-				this.headNode = newNode;
-			} else {
-				this.tailNode.nextNode = newNode;
-			}
-
-			this.tailNode = newNode;
-		}
+		super.pushArrayToTail(valuesArray);
 
 		this.tailNode!.nextNode = this.headNode;
 	}
 
 	public pushToHead(value: T) {
-		const newNode = new SinglyLinkedListNode(value);
-		newNode.nextNode = this.headNode;
-		this.headNode = newNode;
+		super.pushToHead(value);
 
-		if (this.tailNode === undefined) {
-			this.tailNode = newNode;
-		}
-
-		this.tailNode.nextNode = this.headNode;
-	}
-
-	public pushToIndex(index: number, value: T) {
-		if (index <= 1) {
-			throw `Index was less than 0`;
-		}
-
-		if (index === 1) {
-			// just the same as pushing to head
-			this.pushToHead(value);
-		}
-
-		let numberOfNodesSeen = 0;
-		let previousNode = this.headNode!; // at this point we know this isn't pushing to be the head node
-		for (const [currentIndex, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			if (currentIndex === index) {
-				const newNode = new SinglyLinkedListNode(value);
-				previousNode.nextNode = newNode;
-				newNode.nextNode = currentNode;
-				return;
-			} else {
-				previousNode = currentNode;
-				numberOfNodesSeen++;
-			}
-
-			if (currentNode === this.tailNode) {
-				break;
-			}
-		}
-
-		if (index > numberOfNodesSeen + 1) {
-			throw `Provided index, ${index}, is out of range of list with ${numberOfNodesSeen} elements`;
-		}
-
-		// this is just the same as pushing to tail
-		this.pushToTail(value);
+		this.tailNode!.nextNode = this.headNode;
 	}
 
 	public pushToTail(value: T) {
-		const newNode = new SinglyLinkedListNode(value);
+		super.pushToTail(value);
 
-		if (this.tailNode === undefined) {
-			this.headNode = newNode;
-		} else {
-			this.tailNode.nextNode = newNode;
-		}
-
-		this.tailNode = newNode;
-		this.tailNode.nextNode = this.headNode;
-	}
-
-	public size() {
-		let numberOfNodesSeen = 0;
-
-		for (const [_, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			numberOfNodesSeen++;
-
-			if (currentNode === this.tailNode) {
-				break;
-			}
-		}
-
-		return numberOfNodesSeen;
+		this.tailNode!.nextNode = this.headNode;
 	}
 }
