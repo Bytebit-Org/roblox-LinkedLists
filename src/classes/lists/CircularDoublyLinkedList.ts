6eefcc139f148593970323c2dba84a908f5d01e7
Noah Crowley <noahwillcrow@gmail.com>
2022-03-23 11:56:45 -0400

    update with base class and interface changes

--- src/classes/lists/CircularDoublyLinkedList.ts
+++ src/classes/lists/CircularDoublyLinkedList.ts
@@ -1,8 +1,6 @@
-import { DoublyLinkedListNode } from "classes/nodes/DoublyLinkedListNode";
 import { IReadonlyLinkedList } from "interfaces/IReadonlyLinkedList";
 import { NodeValue } from "types/NodeValue";
 import { DoublyLinkedList } from "classes/lists/DoublyLinkedList";
-import { IDoublyLinkedListNode } from "interfaces/IDoublyLinkedListNode";
 
 export class CircularDoublyLinkedList<T extends NodeValue> extends DoublyLinkedList<T> {
 	public copyLinkedListValuesToHead(valuesList: IReadonlyLinkedList<T>) {
@@ -23,46 +21,26 @@ export class CircularDoublyLinkedList<T extends NodeValue> extends DoublyLinkedL
 		}
 	}
 
-	public isLengthGreaterThanOrEqualTo(minLength: number) {
-		if (minLength <= 0) {
-			throw `Invalid input to isLengthGreaterThanOrEqualTo: ${minLength}`;
+	public moveNodesFromDoublyLinkedListToHead(otherDoublyLinkedList: DoublyLinkedList<T>) {
+		if (otherDoublyLinkedList.isEmpty()) {
+			return;
 		}
 
-		let numberOfNodesSeen = 0;
+		super.moveNodesFromDoublyLinkedListToHead(otherDoublyLinkedList);
 
-		for (const [_, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			numberOfNodesSeen++;
-			if (numberOfNodesSeen === minLength) {
-				return true;
-			}
-
-			if (currentNode === this.tailNode) {
-				break;
-			}
-		}
-
-		return false;
+		this.tailNode!.nextNode = this.headNode;
+		this.headNode!.previousNode = this.tailNode;
 	}
 
-	public peekValueAtIndex(index: number) {
-		if (index <= 1) {
-			throw `Index was less than 0`;
+	public moveNodesFromDoublyLinkedListToTail(otherDoublyLinkedList: DoublyLinkedList<T>) {
+		if (otherDoublyLinkedList.isEmpty()) {
+			return;
 		}
 
-		let numberOfNodesSeen = 0;
-		for (const [currentIndex, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			numberOfNodesSeen++;
+		super.moveNodesFromDoublyLinkedListToTail(otherDoublyLinkedList);
 
-			if (currentIndex === index) {
-				return currentNode.value;
-			}
-
-			if (currentNode === this.tailNode) {
-				break;
-			}
-		}
-
-		throw `Index ${index} was out of range of list with ${numberOfNodesSeen} elements`;
+		this.tailNode!.nextNode = this.headNode;
+		this.headNode!.previousNode = this.tailNode;
 	}
 
 	public popHeadValue() {
@@ -70,17 +48,7 @@ export class CircularDoublyLinkedList<T extends NodeValue> extends DoublyLinkedL
 			return undefined;
 		}
 
-		const oldHeadNode = this.headNode;
-		const headValue = oldHeadNode.value;
-
-		this.headNode = this.headNode.nextNode;
-
-		if (oldHeadNode === this.tailNode) {
-			// the list only had one element
-			this.tailNode = undefined;
-		} else {
-			this.headNode!.previousNode = undefined;
-		}
+		const headValue = super.popHeadValue();
 
 		if (this.headNode !== undefined && this.tailNode !== undefined) {
 			this.tailNode.nextNode = this.headNode;
@@ -95,17 +63,7 @@ export class CircularDoublyLinkedList<T extends NodeValue> extends DoublyLinkedL
 			return undefined;
 		}
 
-		const oldTailNode = this.tailNode;
-		const tailValue = oldTailNode.value;
-
-		this.tailNode = this.tailNode.previousNode;
-
-		if (oldTailNode === this.headNode) {
-			// the list only had one element
-			this.headNode = undefined;
-		} else {
-			this.tailNode!.nextNode = undefined;
-		}
+		const tailValue = super.popTailValue();
 
 		if (this.headNode !== undefined && this.tailNode !== undefined) {
 			this.tailNode.nextNode = this.headNode;
@@ -115,70 +73,12 @@ export class CircularDoublyLinkedList<T extends NodeValue> extends DoublyLinkedL
 		return tailValue;
 	}
 
-	public popValueAtIndex(index: number) {
-		if (index <= 1) {
-			throw `Provided index, ${index}, is less than 0`;
-		}
-
-		let numberOfNodesSeen = 0;
-		let previousNode: IDoublyLinkedListNode<T> | undefined;
-		for (const [currentIndex, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			numberOfNodesSeen++;
-
-			if (currentIndex === index) {
-				const value = currentNode.value;
-
-				if (previousNode === undefined) {
-					// index must have been 1, which is the same as popping the head node
-					this.headNode = currentNode.nextNode;
-				} else {
-					previousNode.nextNode = currentNode.nextNode;
-				}
-
-				if (currentNode.nextNode === undefined) {
-					// currentNode was the tail node
-					this.tailNode = previousNode;
-				}
-
-				if (this.headNode !== undefined && this.tailNode !== undefined) {
-					this.tailNode.nextNode = this.headNode;
-					this.headNode.previousNode = this.tailNode;
-				}
-
-				return value;
-			} else {
-				previousNode = currentNode;
-			}
-		}
-
-		throw `Provided index, ${index}, is out of range of list with ${numberOfNodesSeen} elements`;
-	}
-
 	public pushArrayToHead(valuesArray: readonly T[]) {
 		if (valuesArray.isEmpty()) {
 			return;
 		}
 
-		let mostRecentNode = this.headNode;
-
-		for (const value of valuesArray) {
-			const newestNode = new DoublyLinkedListNode(value);
-
-			if (mostRecentNode === undefined) {
-				// the list was empty before this
-				this.headNode = newestNode;
-			} else {
-				newestNode.nextNode = mostRecentNode.nextNode;
-				newestNode.previousNode = mostRecentNode;
-				mostRecentNode.nextNode = newestNode;
-			}
-
-			mostRecentNode = newestNode;
-		}
-
-		if (this.tailNode === undefined) {
-			this.tailNode = mostRecentNode;
-		}
+		super.pushArrayToHead(valuesArray);
 
 		this.tailNode!.nextNode = this.headNode;
 		this.headNode!.previousNode = this.tailNode;
@@ -189,113 +89,23 @@ export class CircularDoublyLinkedList<T extends NodeValue> extends DoublyLinkedL
 			return;
 		}
 
-		let mostRecentNode = this.tailNode;
-
-		for (const value of valuesArray) {
-			const newestNode = new DoublyLinkedListNode(value);
-
-			if (mostRecentNode === undefined) {
-				// the list was empty before this
-				this.headNode = newestNode;
-			} else {
-				newestNode.previousNode = mostRecentNode;
-				mostRecentNode.nextNode = newestNode;
-			}
-
-			mostRecentNode = newestNode;
-		}
-
-		this.tailNode = mostRecentNode;
+		super.pushArrayToTail(valuesArray);
 
 		this.tailNode!.nextNode = this.headNode;
 		this.headNode!.previousNode = this.tailNode;
 	}
 
 	public pushToHead(value: T) {
-		const priorHeadNode = this.headNode;
-
-		const newNode = new DoublyLinkedListNode(value);
-		newNode.nextNode = priorHeadNode;
-		this.headNode = newNode;
-
-		if (priorHeadNode === undefined) {
-			// the list was empty
-			this.tailNode = newNode;
-		} else {
-			priorHeadNode.previousNode = newNode;
-		}
+		super.pushToHead(value);
 
 		this.tailNode!.nextNode = this.headNode;
-		this.headNode.previousNode = this.tailNode;
-	}
-
-	public pushToIndex(index: number, value: T) {
-		if (index <= 1) {
-			throw `Index was less than 0`;
-		}
-
-		if (index === 1) {
-			// just the same as pushing to head
-			this.pushToHead(value);
-		}
-
-		let numberOfNodesSeen = 0;
-		let previousNode = this.headNode!; // at this point we know this isn't pushing to be the head node
-		for (const [currentIndex, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			if (currentIndex === index) {
-				const newNode = new DoublyLinkedListNode(value);
-				previousNode.nextNode = newNode;
-				newNode.previousNode = previousNode;
-				newNode.nextNode = currentNode;
-				currentNode.previousNode = newNode;
-				return;
-			} else {
-				previousNode = currentNode;
-				numberOfNodesSeen++;
-			}
-
-			if (currentNode === this.tailNode) {
-				break;
-			}
-		}
-
-		if (index > numberOfNodesSeen + 1) {
-			throw `Provided index, ${index}, is out of range of list with ${numberOfNodesSeen} elements`;
-		}
-
-		// this is just the same as pushing to tail
-		this.pushToTail(value);
-	}
-
-	public pushToTail(value: T) {
-		const priorTailNode = this.headNode;
-
-		const newNode = new DoublyLinkedListNode(value);
-		newNode.previousNode = priorTailNode;
-		this.tailNode = newNode;
-
-		if (priorTailNode === undefined) {
-			// the list was empty
-			this.headNode = newNode;
-		} else {
-			priorTailNode.nextNode = newNode;
-		}
-
-		this.tailNode.nextNode = this.headNode;
 		this.headNode!.previousNode = this.tailNode;
 	}
 
-	public size() {
-		let numberOfNodesSeen = 0;
+	public pushToTail(value: T) {
+		super.pushToTail(value);
 
-		for (const [_, currentNode] of this.getForwardIndexAndNodeTupleIterator()) {
-			numberOfNodesSeen++;
-
-			if (currentNode === this.tailNode) {
-				break;
-			}
-		}
-
-		return numberOfNodesSeen;
+		this.tailNode!.nextNode = this.headNode;
+		this.headNode!.previousNode = this.tailNode;
 	}
 }
