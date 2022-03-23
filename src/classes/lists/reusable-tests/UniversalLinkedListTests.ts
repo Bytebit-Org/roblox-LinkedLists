0a093db098b6fb3c8790522b67d22648a2c5bb99
Noah Crowley <noahwillcrow@gmail.com>
2022-03-23 13:30:59 -0400

    add more tests for copying linked list values

--- src/classes/lists/reusable-tests/UniversalLinkedListTests.ts
+++ src/classes/lists/reusable-tests/UniversalLinkedListTests.ts
@@ -71,6 +71,164 @@ export function runUniversalLinkedListTests(
 			expect(list2.peekValueAtHead()).to.equal(arrayInput2[0]);
 			expect(list2.peekValueAtTail()).to.equal(arrayInput2[2]);
 		});
+
+		it("copyLinkedListValuesToHead - should keep elements in order", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const arrayInput2 = ["d", "e", "f"];
+			const list2 = createList();
+			list2.pushArrayToHead(arrayInput2);
+
+			list1.copyLinkedListValuesToHead(list2);
+
+			for (const [index, value] of list1.getForwardIterator()) {
+				if (index <= arrayInput2.size()) {
+					expect(value).to.equal(arrayInput2[index - 1]);
+				} else {
+					expect(value).to.equal(arrayInput1[index - arrayInput2.size() - 1]);
+				}
+			}
+		});
+	});
+
+	describe("copyLinkedListValuesToIndex", () => {
+		it("copyLinkedListValuesToIndex - should do nothing if the input list is empty", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const list2 = createList();
+
+			list1.copyLinkedListValuesToIndex(2, list2);
+
+			expect(list1.size()).to.equal(arrayInput1.size());
+			expect(list1.peekValueAtHead()).to.equal(arrayInput1[0]);
+			expect(list1.peekValueAtTail()).to.equal(arrayInput1[2]);
+		});
+
+		it("copyLinkedListValuesToIndex - should change head value when given index 1 and change size without affecting the input list", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const arrayInput2 = ["d", "e", "f"];
+			const list2 = createList();
+			list2.pushArrayToHead(arrayInput2);
+
+			list1.copyLinkedListValuesToIndex(1, list2);
+
+			expect(list1.size()).to.equal(arrayInput1.size() + arrayInput2.size());
+			expect(list1.peekValueAtHead()).to.equal(arrayInput2[0]);
+			expect(list1.peekValueAtTail()).to.equal(arrayInput1[2]);
+
+			expect(list2.size()).to.equal(arrayInput2.size());
+			expect(list2.peekValueAtHead()).to.equal(arrayInput2[0]);
+			expect(list2.peekValueAtTail()).to.equal(arrayInput2[2]);
+		});
+
+		it("copyLinkedListValuesToIndex - should not change head or tail when given an index in the middle and change size without affecting the input list", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const arrayInput2 = ["d", "e", "f"];
+			const list2 = createList();
+			list2.pushArrayToHead(arrayInput2);
+
+			list1.copyLinkedListValuesToIndex(2, list2);
+
+			expect(list1.size()).to.equal(arrayInput1.size() + arrayInput2.size());
+			expect(list1.peekValueAtHead()).to.equal(arrayInput1[0]);
+			expect(list1.peekValueAtTail()).to.equal(arrayInput1[2]);
+
+			expect(list2.size()).to.equal(arrayInput2.size());
+			expect(list2.peekValueAtHead()).to.equal(arrayInput2[0]);
+			expect(list2.peekValueAtTail()).to.equal(arrayInput2[2]);
+		});
+
+		it("copyLinkedListValuesToIndex - should change tail value when given index size + 1 and change size without affecting the input list", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const arrayInput2 = ["d", "e", "f"];
+			const list2 = createList();
+			list2.pushArrayToHead(arrayInput2);
+
+			list1.copyLinkedListValuesToIndex(arrayInput1.size() + 1, list2);
+
+			expect(list1.size()).to.equal(arrayInput1.size() + arrayInput2.size());
+			expect(list1.peekValueAtHead()).to.equal(arrayInput1[0]);
+			expect(list1.peekValueAtTail()).to.equal(arrayInput2[2]);
+
+			expect(list2.size()).to.equal(arrayInput2.size());
+			expect(list2.peekValueAtHead()).to.equal(arrayInput2[0]);
+			expect(list2.peekValueAtTail()).to.equal(arrayInput2[2]);
+		});
+
+		it("copyLinkedListValuesToIndex - should keep elements in order when given index 1", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const arrayInput2 = ["d", "e", "f"];
+			const list2 = createList();
+			list2.pushArrayToHead(arrayInput2);
+
+			list1.copyLinkedListValuesToIndex(1, list2);
+
+			for (const [index, value] of list1.getForwardIterator()) {
+				if (index <= arrayInput2.size()) {
+					expect(value).to.equal(arrayInput2[index - 1]);
+				} else {
+					expect(value).to.equal(arrayInput1[index - arrayInput2.size() - 1]);
+				}
+			}
+		});
+
+		it("copyLinkedListValuesToIndex - should keep elements in order when given an index in the middle", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const arrayInput2 = ["d", "e", "f"];
+			const list2 = createList();
+			list2.pushArrayToHead(arrayInput2);
+
+			list1.copyLinkedListValuesToIndex(2, list2);
+
+			for (const [index, value] of list1.getForwardIterator()) {
+				if (index === 1) {
+					expect(value).to.equal(arrayInput1[0]);
+				} else if (index <= arrayInput2.size() + 1) {
+					expect(value).to.equal(arrayInput2[index - 2]);
+				} else {
+					expect(value).to.equal(arrayInput1[index - arrayInput2.size() - 1]);
+				}
+			}
+		});
+
+		it("copyLinkedListValuesToIndex - should keep elements in order when given index size + 1", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const arrayInput2 = ["d", "e", "f"];
+			const list2 = createList();
+			list2.pushArrayToHead(arrayInput2);
+
+			list1.copyLinkedListValuesToIndex(arrayInput1.size() + 1, list2);
+
+			for (const [index, value] of list1.getForwardIterator()) {
+				if (index <= arrayInput1.size()) {
+					expect(value).to.equal(arrayInput1[index - 1]);
+				} else {
+					expect(value).to.equal(arrayInput2[index - arrayInput1.size() - 1]);
+				}
+			}
+		});
 	});
 
 	describe("copyLinkedListValuesToTail", () => {
@@ -106,6 +264,26 @@ export function runUniversalLinkedListTests(
 			expect(list2.size()).to.equal(arrayInput2.size());
 			expect(list2.peekValueAtHead()).to.equal(arrayInput2[0]);
 			expect(list2.peekValueAtTail()).to.equal(arrayInput2[2]);
+		});
+
+		it("copyLinkedListValuesToTail - should keep elements in order", () => {
+			const arrayInput1 = ["a", "b", "c"];
+			const list1 = createList();
+			list1.pushArrayToHead(arrayInput1);
+
+			const arrayInput2 = ["d", "e", "f"];
+			const list2 = createList();
+			list2.pushArrayToHead(arrayInput2);
+
+			list1.copyLinkedListValuesToTail(list2);
+
+			for (const [index, value] of list1.getForwardIterator()) {
+				if (index <= arrayInput1.size()) {
+					expect(value).to.equal(arrayInput1[index - 1]);
+				} else {
+					expect(value).to.equal(arrayInput2[index - arrayInput1.size() - 1]);
+				}
+			}
 		});
 	});
 }
