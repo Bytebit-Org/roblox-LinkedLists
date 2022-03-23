533296dd8c5ab04230d44a7cc0ab5ae468de1764
Noah Crowley <noahwillcrow@gmail.com>
2022-03-23 12:24:52 -0400

    add clear tests

--- src/classes/lists/reusable-tests/UniversalLinkedListTests.ts
+++ src/classes/lists/reusable-tests/UniversalLinkedListTests.ts
@@ -6,4 +6,34 @@ export function runUniversalLinkedListTests(
 	describe: (phrase: string, callback: () => void) => void,
 	it: (phrase: string, callback: () => void) => void,
 	expect: <T>(value: T) => Expectation<T>,
-) {}
+) {
+	describe("clear", () => {
+		it("should set head and tail nodes to nil", () => {
+			const arrayInput = [{}, {}, {}];
+
+			const list = createList();
+			list.pushArrayToHead(arrayInput);
+
+			expect(list.peekValueAtHead()).to.equal(arrayInput[0]);
+			expect(list.peekValueAtTail()).to.equal(arrayInput[2]);
+
+			list.clear();
+
+			expect(list.peekValueAtHead()).to.never.be.ok();
+			expect(list.peekValueAtTail()).to.never.be.ok();
+		});
+
+		it("should reset size", () => {
+			const arrayInput = [{}, {}, {}];
+
+			const list = createList();
+			list.pushArrayToHead(arrayInput);
+
+			expect(list.size()).to.equal(arrayInput.size());
+
+			list.clear();
+
+			expect(list.size()).to.equal(0);
+		});
+	});
+}
