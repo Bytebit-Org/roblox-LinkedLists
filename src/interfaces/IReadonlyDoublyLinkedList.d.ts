44d99d86acf080a943eb019a8ec44a6aac9d0d10
Noah Crowley <noahwillcrow@gmail.com>
2022-03-23 11:45:09 -0400

    Add getForwardIterator back in

--- src/interfaces/IReadonlyDoublyLinkedList.d.ts
+++ src/interfaces/IReadonlyDoublyLinkedList.d.ts
@@ -4,6 +4,13 @@ import { IReadonlyLinkedList } from "interfaces/IReadonlyLinkedList";
 export interface IReadonlyDoublyLinkedList<T extends NodeValue> extends IReadonlyLinkedList<T> {
 	/**
 	 * Gets an iterator to be used in making looping over values in the list possible.
+	 * Will return lua tuples of the 1-based index and the value.
+	 * Iterator will loop from tail value to head value.
+	 */
+	getForwardIterator(): IterableFunction<LuaTuple<[number, T]>>;
+
+	/**
+	 * Gets an iterator to be used in making looping over values in the list possible.
 	 * Will just return each value.
 	 * Iterator will loop from tail value to head value.
 	 */
