435612235c7b6a74c8652e7439eb95a5420a0448
Noah Crowley <noahwillcrow@gmail.com>
2022-03-23 11:45:31 -0400

    whoops rename to getBackwardIterator

--- src/interfaces/IReadonlyDoublyLinkedList.d.ts
+++ src/interfaces/IReadonlyDoublyLinkedList.d.ts
@@ -7,7 +7,7 @@ export interface IReadonlyDoublyLinkedList<T extends NodeValue> extends IReadonl
 	 * Will return lua tuples of the 1-based index and the value.
 	 * Iterator will loop from tail value to head value.
 	 */
-	getForwardIterator(): IterableFunction<LuaTuple<[number, T]>>;
+	getBackwardIterator(): IterableFunction<LuaTuple<[number, T]>>;
 
 	/**
 	 * Gets an iterator to be used in making looping over values in the list possible.
