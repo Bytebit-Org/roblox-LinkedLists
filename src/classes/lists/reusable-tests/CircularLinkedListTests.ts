cc5f21ccb7977e36fab3a2f9d1be1179c350f4a4
Noah Crowley <noahwillcrow@gmail.com>
2022-03-24 00:49:17 -0400

    add circular copyValuesToSubList tests

--- src/classes/lists/reusable-tests/CircularLinkedListTests.ts
+++ src/classes/lists/reusable-tests/CircularLinkedListTests.ts
@@ -8,7 +8,51 @@ export function runCircularLinkedListTests(
 	expect: <T>(value: T) => Expectation<T>,
 ) {
 	describe("copyValuesToSubList", () => {
-		warn("Not implemented");
+		it("copyValuesToSubList - should throw if startIndex is below 1", () => {
+			const list = createList();
+
+			const arrayInput = ["a", "b", "c"];
+			list.pushArrayToHead(arrayInput);
+
+			expect(() => list.copyValuesToSubList(0, arrayInput.size())).to.throw();
+		});
+
+		it("copyValuesToSubList - should throw if endIndex is too large", () => {
+			const list = createList();
+
+			const arrayInput = ["a", "b", "c"];
+			list.pushArrayToHead(arrayInput);
+
+			expect(() => list.copyValuesToSubList(1, arrayInput.size() + 1)).to.throw();
+		});
+
+		it("copyValuesToSubList - should return a list of the expected size and values that is circular", () => {
+			const list = createList();
+
+			const arrayInput = ["a", "b", "c", "d"];
+			list.pushArrayToHead(arrayInput);
+
+			for (let startIndex = 1; startIndex < arrayInput.size(); startIndex++) {
+				for (let endIndex = startIndex; endIndex <= arrayInput.size(); endIndex++) {
+					const subList = list.copyValuesToSubList(startIndex, endIndex);
+
+					expect(subList.size()).to.equal(endIndex - startIndex + 1);
+
+					for (const [listIndex, value] of subList.getForwardIterator()) {
+						expect(value).to.equal(arrayInput[startIndex - 1 + listIndex - 1]);
+					}
+
+					let numberOfItemsFromSubListValuesIterator = 0;
+					for (const _ of subList.getForwardValuesIterator()) {
+						if (++numberOfItemsFromSubListValuesIterator === subList.size() * 11) {
+							break;
+						}
+					}
+
+					expect(numberOfItemsFromSubListValuesIterator).to.equal(subList.size() * 11);
+				}
+			}
+		});
 	});
 
 	describe("getForwardIterator", () => {
