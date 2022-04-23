9d754b35cafbbd06dbefcd8fa784cd93a1e043ce
Noah Crowley <noahwillcrow@gmail.com>
2022-04-23 10:29:25 -0700

    make IDoublyLinkedList work again

--- src/interfaces/IDoublyLinkedList.d.ts
+++ src/interfaces/IDoublyLinkedList.d.ts
@@ -15,6 +15,26 @@ export interface IDoublyLinkedList<T extends defined> extends ILinkedList<T>, IR
 	): IDoublyLinkedList<T>;
 
 	/**
+	 * {@inheritdoc IReadonlyLinkedList.find}
+	 */
+	find<S extends T>(predicate: (value: T, index: number, list: IDoublyLinkedList<T>) => value is S): S | undefined;
+
+	/**
+	 * {@inheritdoc IReadonlyLinkedList.find}
+	 */
+	find(predicate: (value: T, index: number, list: IDoublyLinkedList<T>) => boolean | undefined): T | undefined;
+
+	/**
+	 * {@inheritdoc IReadonlyLinkedList.findIndex}
+	 */
+	findIndex(predicate: (value: T, index: number, list: IDoublyLinkedList<T>) => boolean | undefined): number;
+
+	/**
+	 * {@inheritdoc IReadonlyLinkedList.forEach}
+	 */
+	forEach(callback: (value: T, index: number, list: IDoublyLinkedList<T>) => void): void;
+
+	/**
 	 * {@inheritdoc IReadonlyLinkedList.map}
 	 */
 	map<U extends defined>(
@@ -25,6 +45,28 @@ export interface IDoublyLinkedList<T extends defined> extends ILinkedList<T>, IR
 	 * {@inheritdoc ILinkedList.popValuesToSubList}
 	 */
 	popValuesToSubList(startIndex: number, endIndex: number): IDoublyLinkedList<T>;
+
+	/**
+	 * {@inheritdoc IReadonlyLinkedList.reduce}
+	 */
+	reduce(
+		this: ReadonlyArray<defined>,
+		callback: (accumulator: T, currentValue: T, currentIndex: number, list: IDoublyLinkedList<T>) => T,
+	): T;
+
+	/**
+	 * {@inheritdoc IReadonlyLinkedList.reduce}
+	 */
+	reduce<U>(
+		this: ReadonlyArray<defined>,
+		callback: (accumulator: U, currentValue: T, currentIndex: number, list: IDoublyLinkedList<T>) => U,
+		initialValue: U,
+	): U;
+
+	/**
+	 * {@inheritdoc IReadonlyLinkedList.some}
+	 */
+	some(callback: (value: T, index: number, list: IDoublyLinkedList<T>) => boolean | undefined): boolean;
 
 	/**
 	 * {@inheritdoc ILinkedList.sort}
